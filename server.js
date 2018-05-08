1d621c77bfbf5c3310aa9e0ca24aaeed68a8ba3f
Glitch (image-search-abstraction-layer-app) <none>
2018-05-08 22:23:22 +0000

    ⛄️🐩 Checkpoint ./server.js:1033623/616

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var request = require("request");
 var key, cx, theUrl, theQuery;
 debugger;
 app.use(express.static('public'));
@@ -10,13 +11,11 @@ app.get("/", function (req, res) {
 app.use("/api/imagesearch/", function(req, res, next){
    key = process.env.KEY, cx = process.env.CX,theUrl = 'https://www.googleapis.com/customsearch/v1?key=' + key + "&cx=" + cx + "&q=",
   theQuery = req.url.split('/')[1];
+  request(theUrl + theQuery, function(error, response, body) {
+  console.log(body);
   res.send(theUrl + theQuery); 
-  next();  
-},function hey(){
-  console.log(theUrl + theQuery);
-}
-);
-
+});
+});
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 }); 
