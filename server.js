eeea4282fcf67667dad1839d91f75e8489974862
Glitch (image-search-abstraction-layer-app) <none>
2018-05-08 21:01:03 +0000

    🍩👓 Checkpoint ./server.js:1033623/86

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var app = express();
 app.use(express.static('public'));
 
 app.get("/", function (request, response) {
+    console.log("HEY");
   response.sendFile(__dirname + '/views/index.html');
 });
-app.use('api/imagesearch/', function(req, res){
+app.use("api/imagesearch/", function(req, res){
+  console.log("HEY");
   res.send("hola");
   
 });
