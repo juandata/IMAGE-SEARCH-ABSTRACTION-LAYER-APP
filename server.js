3d7493638cbb32d307e73318078c672dc8a34e69
Glitch (image-search-abstraction-layer-app) <none>
2018-05-09 18:44:08 +0000

    🎊🚙 Checkpoint ./server.js:1033623/650

--- server.js
+++ server.js
@@ -9,29 +9,23 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 app.use("/api/imagesearch/", function(req, res, next){
-   key = process.env.KEY, cx = process.env.CX,theUrl = 'https://www.googleapis.com/customsearch/v1?key=' + key + "&cx=" + cx + "&q=",
+   key = process.env.KEY, cx = process.env.CX,theUrl = 'https://www.googleapis.com/customsearch/v1?key=' + key + "&cx=" + cx + "&searchType=image&q=",
   theQuery = req.url.split('/')[1];
   request(theUrl + theQuery, function(error, response, body) {
   var respinJson = JSON.parse(body);
   var respList = {
-    0 : ""
+  0: {}, 1: {}
   };
       res.json(respinJson); 
-      console.log(theUrl + theQuery);
 for(var i=0; i < respinJson.items.length; i ++){
   //console.log(respinJson.items[i].link, respinJson.items[i].snippet, respinJson.items[i].pagemap.metatags[0].cse_thumbnail[0].src);
   /*if(respinJson.items[i].pagemap.metatags[0].length != 3){
       console.log("not equal to 3");
        }*/
-    console.log( respinJson.items[i].pagemap.cse_image[0].src);
-    
-  if( respinJson.items[i].pagemap.cse_thumbnail === undefined){
-      //console.log("no existe la propiedad en la respuesta");
-     }
-  else {
-    //console.log(respinJson.items[i].pagemap.cse_thumbnail[0].src);
-      //console.log(rtypeofespinJson.items[i].pagemap.cse_thumbnail[0].src);
-  }
+    //console.log(respinJson.items[i].link);
+  respList[i].url = respinJson.items[i].link;
+  console.log(respList);  
+
 }
 });
 });
